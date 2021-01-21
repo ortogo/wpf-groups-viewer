c5da01b3416bf952255b83a13e2cabbb7017c9ed
Vladislav Volkov <ortogorbd@gmail.com>
2021-01-22 01:31:18 +0200

    Create a basic project

---
+++ Wpf-Groups-Viewer/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace WpfGroupsViewer
+{
+    public sealed class Constants
+    {
+        /// <summary>
+        /// Default resources directory name.
+        /// </summary>
+        public const string RESOURCES_DIRECTORY_NAME = "Resources";
+
+        /// <summary>
+        /// Text file format extension.
+        /// </summary>
+        public const string TXT_EXTENSION = ".txt";
+    }
+}
---
+++ Wpf-Groups-Viewer/UI/Models/GroupModel.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using WpfGroupsViewer.Helpers;
+
+namespace WpfGroupsViewer.UI.Models
+{
+    public class GroupModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a group name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a group number.
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Gets a path to directory that contains group details file.
+        /// </summary>
+        public string DetailsFilePath =>
+            Path.Combine(
+                AssemblyHelper.GetAssemblyDirectoryPath(),
+                Constants.RESOURCES_DIRECTORY_NAME,
+                $"{Name}{Constants.TXT_EXTENSION}");
+
+        #endregion
+    }
+}
